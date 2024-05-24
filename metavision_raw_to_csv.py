adc9b3fb2ed5f7c05e5665f6d7dd1f17509fd1a6
Lachlan Chen <lachlan@lazying.art>
2024-05-24 20:55:46 +0800

    update metavision raw to csv conversion

--- metavision_raw_to_csv.py
+++ metavision_raw_to_csv.py
@@ -1,54 +1,50 @@
-# Copyright (c) Prophesee S.A.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
-# Unless required by applicable law or agreed to in writing, software distributed under the License is distributed
-# on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and limitations under the License.
-
-"""
-Metavision RAW or DAT to CSV python sample.
-"""
-
-from metavision_core.event_io import EventsIterator
+import sys
+print(sys.path)
+# Add /usr/lib/python3/dist-packages/ to PYTHONPATH if the output of print(sys.path) does not mention it.
+sys.path.append("/usr/lib/python3/dist-packages/")
 import os
+import argparse
+from metavision_core.event_io import EventsIterator
 from tqdm import tqdm
 
-
 def parse_args():
-    import argparse
     """Parse command line arguments."""
-    parser = argparse.ArgumentParser(description='Metavision RAW or DAT to CSV.',
+    parser = argparse.ArgumentParser(description='Convert Metavision RAW or DAT to CSV format.',
                                      formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument('-i', '--input-file', dest='input_path',  help="Path to input RAW/DAT file")
-    parser.add_argument('-o', '--output-dir', help="Path to csv output directory")
-    parser.add_argument('-s', '--start-ts', type=int, default=0, help="start time in microsecond")
-    parser.add_argument('-d', '--max-duration', type=int, default=1e6 * 60, help="maximum duration in microsecond")
-    parser.add_argument('--delta-t', type=int, default=1000000, help="Duration of served event slice in us.")
-    args = parser.parse_args()
-    return args
-
+    parser.add_argument('-i', '--input-file', dest='input_path', required=True,
+                        help="Path to input RAW or DAT file.")
+    parser.add_argument('-o', '--output-dir', default=None,
+                        help="Directory to save the output CSV file. Defaults to the input file's directory.")
+    parser.add_argument('-s', '--start-ts', type=int, default=0,
+                        help="Start time in microseconds.")
+    parser.add_argument('-d', '--max-duration', type=int, default=int(1e6 * 60),
+                        help="Maximum duration in microseconds.")
+    parser.add_argument('--delta-t', type=int, default=1000000,
+                        help="Duration of served event slice in microseconds.")
+    return parser.parse_args()
 
 def main(args):
-    if os.path.isfile(args.input_path):
-        output_file = os.path.join(args.output_dir, os.path.basename(args.input_path)[:-4] + ".csv")
-    else:
-        raise TypeError(f'Fail to access file: {args.input_path}')
+    if not os.path.isfile(args.input_path):
+        raise FileNotFoundError(f"Input file not found: {args.input_path}")
 
-    mv_iterator = EventsIterator(input_path=args.input_path, delta_t=args.delta_t, start_ts=args.start_ts,
-                                 max_duration=args.max_duration)
+    if args.output_dir is None:
+        args.output_dir = os.path.dirname(args.input_path)
+
+    if not os.path.exists(args.output_dir):
+        os.makedirs(args.output_dir, exist_ok=True)
+
+    output_file = os.path.join(args.output_dir, os.path.basename(args.input_path)[:-4] + ".csv")
+    mv_iterator = EventsIterator(input_path=args.input_path, delta_t=args.delta_t,
+                                 start_ts=args.start_ts, max_duration=args.max_duration)
 
     with open(output_file, 'w') as csv_file:
+        # Write header
+        csv_file.write("event_timestamp,x,y,polarity\n")
         for evs in tqdm(mv_iterator, total=args.max_duration // args.delta_t):
-            for (x, y, p, t) in evs:
-                csv_file.write("%d,%d,%d,%d\n" % (x, y, p, t))
-
+            for ev in evs:
+                csv_file.write(f"{ev['t']},{ev['x']},{ev['y']},{ev['p']}\n")
 
 if __name__ == "__main__":
     args = parse_args()
-    sequence_filename_raw = "E:/pythonProject/NeuroDH/data/evk/recording_2024-04-29_16-41-18.raw"
-    args.input_path = sequence_filename_raw
-    args.output_dir = "E:/pythonProject/NeuroDH/data/evk/"
     main(args)
 
